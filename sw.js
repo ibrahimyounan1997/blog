da642087653415518191b8b4f88ff243e2b4e776
Ibrahim Younan <71520071+iniy3@users.noreply.github.com>
2021-07-28 15:21:21 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,13 @@
-self.addEventListener('install', e => { console.log('PWA Service Worker installing.');  let timeStamp = Date.now(); e.waitUntil(  caches.open('blog_service_worker').then(cache => {  return cache.addAll([ 'https://testing-bog-2.blogspot.com/index.html',  'https://1.bp.blogspot.com/-EwtxObb4h0o/Xwyu5OZmxEI/AAAAAAAAIyM/zLXmFx0fjoIiExohXlcCezvEjeJQgdH5ACK4BGAYYCw/s192/iaos192.png'  ])  .then(() => self.skipWaiting());  })  ) });
-self.addEventListener('activate',  event => { console.log('PWA Service Worker activating.');  event.waitUntil(self.clients.claim());  });
-self.addEventListener('fetch', event => {  event.respondWith( caches.match(event.request).then(response => {  return response || fetch(event.request);  })  );  });
+// cash files
+const cacheName = 'js13kPWA-v1'; const appShellFiles = [
+'https://cdn.statically.io/gh/iniy3/pwa-examples/master/js13kpwa/icons/icon-192.png',
+'https://cdn.statically.io/gh/iniy3/pwa-examples/master/js13kpwa/icons/icon-512.png'
+'https://testing-bog-2.blogspot.com/index.html',
+];
+
+
+// Installing Service Worker
+self.addEventListener('install', (e) => {console.log('[Service Worker] Install');e.waitUntil((async () => { const cache = await caches.open(cacheName); console.log('[Service Worker] Caching all: app shell and content'); await cache.addAll(contentToCache); })());  });
+
+// Fetching content using Service Worker
+self.addEventListener('fetch', (e) => { e.respondWith((async () => { const r = await caches.match(e.request);  console.log(`[Service Worker] Fetching resource: ${e.request.url}`); if (r) return r;  const response = await fetch(e.request);  const cache = await caches.open(cacheName); console.log(`[Service Worker] Caching new resource: ${e.request.url}`); cache.put(e.request, response.clone()); return response; })());  });
