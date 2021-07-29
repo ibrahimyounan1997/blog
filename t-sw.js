2c743cff5afecb3fa62dbc855f3894fbcd89f32c
Ibrahim Younan <71520071+iniy3@users.noreply.github.com>
2021-07-29 05:35:46 +0200

    Update and rename t-sw.js to t-3-sw.js

--- t-sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (e) => { e.waitUntil( caches.open('fox-store').then((cache) => cache.addAll([
-'https://cdn.statically.io/gh/iniy3/pwa-examples/master/js13kpwa/icons/icon-192.png',
-'https://cdn.statically.io/gh/iniy3/pwa-examples/master/js13kpwa/icons/icon-512.png'
-'https://testing-bog-2.blogspot.com/index.html',
-])), ); });
-
-self.addEventListener('fetch', (e) => { console.log(e.request.url);e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)),); });
