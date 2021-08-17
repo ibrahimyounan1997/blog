3a8d7b0e9196b55d4fec7c169dceffde40fc62b3
Ibrahim Younan <71520071+iniy3@users.noreply.github.com>
2021-08-17 14:23:02 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,13 +4,7 @@ const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
-const PRECACHE_URLS = [
-  'https://testing-bog-1.blogspot.com',
-  '*.html',
-  '*.js',
-  '*.css',
-  '*.img',
-];
+const PRECACHE_URLS = [  '*.html',  '*.js',  '*.css',  '*.jpeg',  '*.jpg',  '*.png',  '*.gif',  ];
 
 self.addEventListener('install', event => { event.waitUntil(  caches.open(PRECACHE) .then(cache => cache.addAll(PRECACHE_URLS)) .then(self.skipWaiting()) );  });
 self.addEventListener('activate', event => {  const currentCaches = [PRECACHE, RUNTIME];  event.waitUntil(  caches.keys().then(cacheNames => {  return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));  }).then(cachesToDelete => { return Promise.all(cachesToDelete.map(cacheToDelete => {  return caches.delete(cacheToDelete);  }));  }).then(() => self.clients.claim()) );  });
